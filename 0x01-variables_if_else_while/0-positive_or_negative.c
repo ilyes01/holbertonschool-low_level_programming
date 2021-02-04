916386677887e40c0eea3b0cd42065f10df86680
ilyes <ilyes01>
2021-02-04 12:54:54 +0100

    crows and crown

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *main - entry point 
+ *
+ *return: 0
+ */
 int main(void)
 {
 	int n;
