b28b41c58879e53f5fa6056331a5e98255e63c3e
ilyes <ilyes01>
2021-02-04 16:05:04 +0100

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+*/
+int main(void)
+{
+	unsigned int o;
+
+	for (o = 0; o <= 9; ++o)
+	{
+		printf("%i", o);
+	}
+	putchar('\n');
+	return (0);
+}
