4f85c761515dededcc7ff455257e6e9e046877bf
ilyes <ilyes01>
2021-02-04 15:34:07 +0100

    tbalbiz

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i - 1 < 'z'; ++i)
+	{
+		if ((i != 'q') && (i != 'e'))
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
