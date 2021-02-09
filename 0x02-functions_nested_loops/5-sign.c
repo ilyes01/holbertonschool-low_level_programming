e589bdfe0276d50860af1a9327647a9cc4fa0285
ilyes <ilyes01>
2021-02-09 11:08:20 +0100

    ooo

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_sign - sign of number
+ * @n : parameter
+ * Return: 0,1 or -1
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n <  0)
+{
+_putchar('-');
+return (-1);
+}
+else
+_putchar('0');
+return ('0');
+}
