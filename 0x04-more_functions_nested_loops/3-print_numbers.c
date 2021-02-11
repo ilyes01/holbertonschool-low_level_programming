46ec9086add51da77cbd16a5c2e11f8af998b9de
ilyes <ilyes01>
2021-02-11 10:46:51 +0100

    prit num

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_numbers - print from 0 to 9 followed by new line
+*
+*Return: 0
+*/
+void print_numbers(void)
+{
+_putchar('0');
+_putchar('1');
+_putchar('2');
+_putchar('3');
+_putchar('4');
+_putchar('5');
+_putchar('6');
+_putchar('7');
+_putchar('8');
+_putchar('9');
+_putchar('\n');
+}
