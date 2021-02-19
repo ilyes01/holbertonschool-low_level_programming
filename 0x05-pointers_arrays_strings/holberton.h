9de1a0c6243b76e1483234c9fcd10753e821feeb
ilyes <ilyes01>
2021-02-19 10:16:03 +0100

    one for all

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,7 +1,6 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-<<<<<<< HEAD
 void print_rev(char *s);
 int _putchar(char c);
 void rev_string(char *s);
@@ -9,7 +8,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-=======
 void _puts(char *str);
 int _putchar(char c);
->>>>>>> 69fe07c51174282bf155157ad29af818b74e7770
