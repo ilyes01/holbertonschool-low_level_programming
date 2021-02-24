85f9d8ee98bce7319b934ac905979e6b3a8bbe20
ilyes <ilyes01>
2021-02-24 11:06:58 +0100

    willcamp

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*int wildcmp - function
+*@s1: char
+*@s2: char
+* Return: 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == *s2 && *s1 != '\0')
+return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == '\0')
+{
+if (*s2 == '\0')
+return (1);
+if (*s2 == '*')
+return (wildcmp(s1, s2 + 1));
+}
+if (*s2 == '*')
+return ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1)));
+return (0);
+}
