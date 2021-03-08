6cc8a7675eccd33a344e630fed80f8b0fccfb651
ilyes <ilyes01>
2021-03-08 11:58:55 +0100

    kibgv

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+*struct dog - dogs
+*@name: name
+*@age: age
+*@owner: owner
+**/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
