8ea6921ed7bd59660393c4522d4447b8508e868d
ilyes <ilyes01>
2021-03-08 12:17:59 +0100

    sgh

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,20 @@
-#ifndef DOG_HEADER
-#define DOG_HEADER
+#ifndef DOGHEADER
+#define DOGHEADER
 /**
-*struct dog - dogs
-*@name: name
-*@age: age
-*@owner: owner
+* struct dog - structure for dogs
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
 **/
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
