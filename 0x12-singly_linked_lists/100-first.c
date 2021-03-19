fb0f8154f16fe1b18ab1d31eaa9a8360eba153c3
ilyes <ilyes01>
2021-03-19 10:22:25 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void function(void) __attribute__ ((constructor));
+/**
+*function - singly link
+*
+*/
+void function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
