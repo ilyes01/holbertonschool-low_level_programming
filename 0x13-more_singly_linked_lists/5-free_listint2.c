636ce17c4f8ed31556454f831ad4b551ddbac2f1
ilyes <ilyes01>
2021-03-23 12:44:57 +0100

    freesss

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*free_listint2 -  function that frees
+*
+*@head: pointer
+**/
+void free_listint2(listint_t **head)
+{
+
+	listint_t *tmp;
+	if (!head)
+		return;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free((*head));
+		(*head) = tmp;
+	}
+}
