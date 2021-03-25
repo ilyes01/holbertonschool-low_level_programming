fe2244c8cf6a0401659bf29c57c4248274ca6a2d
ilyes <ilyes01>
2021-03-25 12:28:30 +0100

    getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "holberton.h"
+#include <stdio.h>
+/**
+*get_bit - function that returns the value of a bit at a given index.
+*
+*@n: long int
+*@index: int index
+*Return:0000
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index < 64) 
+{
+return ((n >> index) & 1);
+}
+else
+{
+return (-1);
+}
+}
