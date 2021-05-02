085b18bcfe514375234c45a845f987b1a0697b88
Ilyes01 <ilyesghrab7@gmail.com>
2021-05-02 12:24:24 +0100

    print

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+*print_dlistint - function that print all the elements
+*@h: var
+*Return: number
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	unsigned int i = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	i++;
+	}
+		return (i);
+}
